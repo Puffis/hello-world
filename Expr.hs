71830f98eab5baee21346942acdce38874c6e83a
Siffran <davidaxelbenjaminsson@gmail.com>
2016-10-07 14:26:20 +0200

    Update Expr.hs

--- Expr.hs
+++ Expr.hs
@@ -2,51 +2,75 @@ module Expr where
 
 -- code between "----" markers are not part of the assignment
 
---tjaaaaaaa spagheetiiii
-
-import Test.QuickCheck
- test = 5 + 5
 --------------------------
 import Test.QuickCheck.Gen
 import System.Random
 --------------------------
+
+import Test.QuickCheck
+
+-- A
 
 -- Data types, need to add types for cos- and sin- functions?? 
 
 data Expr =  Num Float
            | Add Expr Expr
            | Mul Expr Expr
-           | Floating Expr
     deriving Eq
+
+-- B
 
 -- instance so we can show expressions
 
 instance Show Expr where
     show = showExpr
 
+showExpr :: Expr -> String
+showExpr (Num n)   = show n
+showExpr (Add a b) = showExpr a ++ " + " ++ showExpr b
+showExpr (Mul a b) = showExpr a ++ " * " ++ showExpr b
+
+{-
+showFactor :: Expr -> String
+showFactor (Add a b) = "(" ++ showExpr (Add a b) ++ ")"
+showFactor e         = showExpr e
+-}
+
+-- C
+
 eval :: Expr -> Float
 eval (Num n)   = n
 eval (Add a b) = eval a + eval b
 eval (Mul a b) = eval a * eval b
 
-showExpr :: Expr -> String
-showExpr (Num n)   = show n
-showExpr (Add a b) = showExpr a ++ " + " ++ showExpr b
-showExpr (Mul a b) = showFactor a ++ " * " ++ showFactor b
+-- D
 
-showFactor :: Expr -> String
-showFactor (Add a b) = "(" ++ showExpr (Add a b) ++ ")"
-showFactor e         = showExpr e
+-- comments?
 
+readExpr :: String -> Maybe Expr
+readExpr = undefined
+
+-- E
+
+-- comments?
+
+-- prop_ShowReadExpr :: Expr -> Bool
+
+-- arbExpr :: Int -> Gen Expr
+
+-- instance Arbitrary Expr where
+--     arbitrary = sized arbExpr
+
+
+--Random code from here and on, ignore!
 ------------------------------------------------------
 
-{-
 questions :: IO ()
 questions = do
             e <- generate arbitrary
             putStr ("What is " ++ show e ++ "?")
             ans <- getLine
-            putStrLn (if read ans == eval e
+            putStrLn (if ans == show (eval e)
                         then "Correct!" 
                         else "Wrong!")
 
@@ -56,10 +80,10 @@ generate g = do
     return (unGen g seed 10)
 
 instance Arbitrary Expr where
-    arbitrary = arbExpr
+    arbitrary = sized arbExpr
 
-arbExpr :: Gen Expr
-arbExpr = 
+arbExpr :: Int -> Gen Expr
+arbExpr s = 
     frequency [(1, do n <- arbitrary
                       return (Num n))
               ,(s, do a <- arbExpr s'
@@ -68,8 +92,7 @@ arbExpr =
               ,(s, do a <- arbExpr s'
                       b <- arbExpr s'
                       return (Mul a b)) ]
-        where
-          s' = s ´div´ 2
+    where
+      s' = s `div` 2
 
 ------------------------------------------------------
--}
